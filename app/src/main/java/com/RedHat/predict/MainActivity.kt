99b42f3b2ba683b16d1bb2409f3ed511c3176525
muardy <muardy.irawan@gmail.com>
2021-05-23 01:35:39 +0700

    Login Register

--- app/src/main/java/com/RedHat/predict/MainActivity.kt
+++ app/src/main/java/com/RedHat/predict/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.RedHat.predict
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.RedHat.predict.databinding.ActivityMainBinding
 import com.RedHat.predict.home.SectionsPagerAdapter
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
     private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.reqtoken))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         activityMainBinding.viewPager.adapter = sectionsPagerAdapter
         activityMainBinding.tabs.setupWithViewPager(activityMainBinding.viewPager)
         supportActionBar?.elevation = 0f
+
+        activityMainBinding.btnLogout.setOnClickListener {
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                val intent= Intent(this, LoginActivity::class.java)
+                Toast.makeText(this,"Logging Out",Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
