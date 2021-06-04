047fd678e017212177f588ffbd75c319dd9c2622
muardy <muardy.irawan@gmail.com>
2021-06-05 00:38:57 +0700

    splash

--- app/src/main/java/com/RedHat/predict/SplashScreen.kt
+++ app/src/main/java/com/RedHat/predict/SplashScreen.kt
@@ -3,13 +3,23 @@ package com.RedHat.predict
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity(){
-
+    private var TIME_OUT:Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.activity_splash)
+        loadSplashScreen()
     }
+
+    private fun loadSplashScreen(){
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },TIME_OUT)
+    }
+
 }
